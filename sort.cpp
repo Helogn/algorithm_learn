da51f7dc959a8066e484a2f4fe2ba7095e7be7be
hejiang <helogn@outlook.com>
2024-05-28 21:49:07 +0800

    2024-5-28 21:48:00 h3c

--- sort.cpp
+++ sort.cpp
@@ -5,54 +5,11 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-vector<int> quickSort(vector<int> vec, int mid_index = 0)
+
+
+vector<int> quickSort(vector<int> &vec, int low , int high)
 {
-    size_t size = vec.size();
-    cout<<"size of vector is "<<size<<endl;
-    if (size <= 1) return vec;
 
-    int mid_value = vec[mid_index];
-    int head_ind = mid_index + 1;
-    int end_ind = size - 1;
-    bool right = true;
-    /*
-    循环条件错误：
-    你的循环条件(head_ind == mid_index) && (end_ind == mid_index)在一开始就是错误的
-    因为这会导致循环永远不会执行，
-    因为head_ind和end_ind初始时就已经等于mid_index。
-    正确的循环应该持续到head_ind和end_ind相遇为止。
-    while((head_ind == mid_index) && (end_ind == mid_index))
-    */
-    while(head_ind == end_ind)
-    {
-        if (right)
-        {
-            if(vec[end_ind] < mid_value)
-            {
-                vec[mid_index] = vec[end_ind];
-                right = false;
-            }
-            else
-            {
-                end_ind--;
-            }
-        }
-        else
-        {
-            if(vec[head_ind] > mid_value)
-            {
-                vec[mid_index] = vec[head_ind];
-                right = true;
-            }
-            else
-            {
-                head_ind++;
-            }
-        }
-    }
-
-    return vec;
-    
 
 }
 
