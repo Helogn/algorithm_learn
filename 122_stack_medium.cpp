52e736739b65c38abb1b6f6ca6132aadf8b07159
hejiang <helogn@outlook.com>
2024-05-30 14:57:56 +0800

    122 pass

--- 122_stack_medium.cpp
+++ 122_stack_medium.cpp
@@ -3,7 +3,8 @@
 medium
 给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
 
-在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+在每一天，你可以决定是否购买和/或出售股票。你在任何时候最多只能持有一股股票。
+你也可以先购买，然后在 同一天 出售。
 
 返回 你能获得的 最大 利润 。
 */
@@ -12,7 +13,7 @@ medium
 #include<vector>
 #include<algorithm>
 #include "com.h"
-
+/*
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -20,23 +21,58 @@ public:
         size_t size = prices.size();
         if (size<=1) return 0;
         
-        int min_price = prices[0];
-        int max_value = 0;
+        int sum_value = 0, min_price = prices[0], max_price = prices[0];
         for(int i = 1; i < size; i++)
         {
-            max_value = max(max_value, prices[i] - min_price);
-            min_price = min(min_price, prices[i]);
+            if(i == size - 1)
+            {
+                if(prices[i] > max_price)
+                {
+                    sum_value += prices[i] - min_price;
+                }
+                else
+                {
+                    sum_value += max_price-min_price;
+                }
+                return sum_value;
+
+            }
+
+            if(max_price > prices[i])
+            {
+                sum_value += max_price - min_price;
+                min_price = prices[i];
+            }
+            max_price = prices[i];
+            
         }
 
-        return max_value;
+        return sum_value;
 
     }
 };
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        size_t size = prices.size();
+        int money = 0;
+        for (int i = 1; i< size; i++)
+        {
+            int minus = prices[i] - prices[i-1];
+            if(minus > 0) money += minus;
+            
+        }
+        return money;
+    }
+};
+
 
 int main()
 {
     // 指针从后循环
-    vector<int> vec = {1,3,5,7,3,4,65,7,8,32,1,2,3,4,2,4};
+    vector<int> vec = {1,2,3,4,5};
 
     Solution res = Solution();
     int max = res.maxProfit(vec);
